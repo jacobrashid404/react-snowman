57488135f3b5efbaea48b9bb1ba947eaefa4feb7
Ricardo Perez <rxperez12@gmail.com>
2024-05-29 16:34:03 -0700

    testing comments

--- src/Snowman.test.jsx
+++ src/Snowman.test.jsx
@@ -2,16 +2,33 @@ import { test, expect } from "vitest";
 import { render, fireEvent } from "@testing-library/react";
 import Snowman from "./Snowman.jsx";
 
-test("image doesn't disappear if more than 6 guesses are made", function () {
-  const { container } = render(<Snowman />);
+test("If loss, can't click on board, and image doesn't crash", function () {
+  const { container } = render(<Snowman />); // TODO: add this to test -> words = ["apple"], maxWrong= 6
   const lettersToCheck = ["w", "x", "y", "z", "b", "c", "k"];
 
-  const $containerElems = lettersToCheck.map((ltr) => {
+  const $containerElems = lettersToCheck.map((ltr) => { //FIXME: fix this test
     const elem = container.querySelector(`.letter_${ltr}`);
-    fireEvent.click(elem);
+    const elem = container.querySelector(`button[value="${ltr}"]`); //CSS VALUE SELECTOR!!!!
+
+    try {
+      fireEvent.click(elem);
+    } catch (err) {
+      expect(err.message).toContain("Unable to fire a");
+    }
     return elem;
   });
 
-  const snowmanImg = container.querySelector(".Snowman-img")
+  const snowmanImg = container.querySelector(".Snowman-img");
   expect(snowmanImg.src).toContain("png");
+});
+
+//TODO: same test but lose at a different value
+
+//TODO: real verification of functionality, do we win?
+
+//TODO: some more testing!
+
+test("snapshot test", function () {
+  const { container } = render(<Snowman />);
+  expect(container).toMatchSnapshot();
 });
