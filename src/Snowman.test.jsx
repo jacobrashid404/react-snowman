18bfb8d6e78c34d388e2fdc25b86e55ad1773364
Ricardo Perez <rxperez12@gmail.com>
2024-05-29 17:40:50 -0700

    added tests for Snowman game

--- src/Snowman.test.jsx
+++ src/Snowman.test.jsx
@@ -3,16 +3,25 @@ import { render, fireEvent } from "@testing-library/react";
 import Snowman from "./Snowman.jsx";
 
 test("If loss, can't click on letter, and image doesn't crash", function () {
-  const { container } = render(<Snowman words={["apple"]} maxWrong={6} />);
+  const { container } = render(
+    <Snowman
+      words={["apple"]}
+      maxWrong={6}
+    />,
+  );
   const lettersToCheck = ["w", "x", "y", "z", "b", "c", "k"];
 
   for (let i = 0; i < lettersToCheck.length - 2; i++) {
-    const elem = container.querySelector(`button[value="${lettersToCheck[i]}"]`); //CSS VALUE SELECTOR!!!!
+    const elem = container.querySelector(
+      `button[value="${lettersToCheck[i]}"]`,
+    ); //CSS VALUE SELECTOR!!!!
     fireEvent.click(elem);
   }
 
   try {
-    const keyToPress = container.querySelector(`button[value="${lettersToCheck[6]}}"]`);
+    const keyToPress = container.querySelector(
+      `button[value="${lettersToCheck[6]}}"]`,
+    );
     fireEvent.click(keyToPress);
   } catch (err) {
     expect(err.message).toContain("Unable to fire a");
@@ -22,18 +31,26 @@ test("If loss, can't click on letter, and image doesn't crash", function () {
   expect(snowmanImg.src).toContain("png");
 });
 
-
 test("Test loss logic with custom maxWrong value", function () {
-  const { container } = render(<Snowman words={["apple"]} maxWrong={3} />);
+  const { container } = render(
+    <Snowman
+      words={["apple"]}
+      maxWrong={3}
+    />,
+  );
   const lettersToCheck = ["w", "x", "y", "z"];
 
   for (let i = 0; i < lettersToCheck.length - 2; i++) {
-    const elem = container.querySelector(`button[value="${lettersToCheck[i]}"]`); //CSS VALUE SELECTOR!!!!
+    const elem = container.querySelector(
+      `button[value="${lettersToCheck[i]}"]`,
+    ); //CSS VALUE SELECTOR!!!!
     fireEvent.click(elem);
   }
 
   try {
-    const keyToPress = container.querySelector(`button[value="${lettersToCheck[6]}}"]`);
+    const keyToPress = container.querySelector(
+      `button[value="${lettersToCheck[6]}}"]`,
+    );
     fireEvent.click(keyToPress);
   } catch (err) {
     expect(err.message).toContain("Unable to fire a");
@@ -42,7 +59,36 @@ test("Test loss logic with custom maxWrong value", function () {
 
 //TODO: real verification of functionality, do we win?
 
+test("test for win", function () {});
+
 //TODO: some more testing!
+
+test("test that bad guess changes picture", function () {
+  const { debug, container } = render(
+    <Snowman
+      maxWrong={6}
+      words={["apple"]}
+    />,
+  );
+  const $letterElem = container.querySelector(`button[value="b"]`);
+  fireEvent.click($letterElem);
+  const $snowmanImage = container.querySelector(".Snowman-img");
+  expect($snowmanImage.src).toContain("/src/1.png");
+});
+
+test("test for correct letter revealed after guess", function () {
+  const { debug, container } = render(
+    <Snowman
+      maxWrong={6}
+      words={["apple"]}
+    />,
+  );
+
+  const $letterElem = container.querySelector(`button[value="a"]`);
+  fireEvent.click($letterElem);
+  const correctGuessValue = container.querySelector(".Snowman-word").innerHTML;
+  expect(correctGuessValue).toContain("a");
+});
 
 test("snapshot test", function () {
   const { container } = render(<Snowman />);
