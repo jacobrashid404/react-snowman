bad8fd9585c141d270d91f96bfc0d7919e592c83
Jacob <jacobrashid404@gmail.com>
2024-05-29 17:14:12 -0700

    refactored loss logic test and added a new test with a different maxWrong val

--- src/Snowman.test.jsx
+++ src/Snowman.test.jsx
@@ -2,27 +2,43 @@ import { test, expect } from "vitest";
 import { render, fireEvent } from "@testing-library/react";
 import Snowman from "./Snowman.jsx";
 
-test("If loss, can't click on board, and image doesn't crash", function () {
-  const { container } = render(<Snowman />); // TODO: add this to test -> words = ["apple"], maxWrong= 6
+test("If loss, can't click on letter, and image doesn't crash", function () {
+  const { container } = render(<Snowman words={["apple"]} maxWrong={6} />);
   const lettersToCheck = ["w", "x", "y", "z", "b", "c", "k"];
 
-  const $containerElems = lettersToCheck.map((ltr) => { //FIXME: fix this test
-    const elem = container.querySelector(`.letter_${ltr}`);
-    const elem = container.querySelector(`button[value="${ltr}"]`); //CSS VALUE SELECTOR!!!!
+  for (let i = 0; i < lettersToCheck.length - 2; i++) {
+    const elem = container.querySelector(`button[value="${lettersToCheck[i]}"]`); //CSS VALUE SELECTOR!!!!
+    fireEvent.click(elem);
+  }
 
-    try {
-      fireEvent.click(elem);
-    } catch (err) {
-      expect(err.message).toContain("Unable to fire a");
-    }
-    return elem;
-  });
+  try {
+    const keyToPress = container.querySelector(`button[value="${lettersToCheck[6]}}"]`);
+    fireEvent.click(keyToPress);
+  } catch (err) {
+    expect(err.message).toContain("Unable to fire a");
+  }
 
   const snowmanImg = container.querySelector(".Snowman-img");
   expect(snowmanImg.src).toContain("png");
 });
 
-//TODO: same test but lose at a different value
+
+test("Test loss logic with custom maxWrong value", function () {
+  const { container } = render(<Snowman words={["apple"]} maxWrong={3} />);
+  const lettersToCheck = ["w", "x", "y", "z"];
+
+  for (let i = 0; i < lettersToCheck.length - 2; i++) {
+    const elem = container.querySelector(`button[value="${lettersToCheck[i]}"]`); //CSS VALUE SELECTOR!!!!
+    fireEvent.click(elem);
+  }
+
+  try {
+    const keyToPress = container.querySelector(`button[value="${lettersToCheck[6]}}"]`);
+    fireEvent.click(keyToPress);
+  } catch (err) {
+    expect(err.message).toContain("Unable to fire a");
+  }
+});
 
 //TODO: real verification of functionality, do we win?
 
